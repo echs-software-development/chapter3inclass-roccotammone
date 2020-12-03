708668214c244c1bf4ddf2b2455d892c8a9b6fea
ROCCO TAMMONE <rt0726@students.necsd.net>
2020-12-03 15:17:17 +0000

    done

--- Prob3.java
+++ Prob3.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
-    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number 0-4: ");
+    int grade = scan.nextInt();
+    if (grade==4)
+    System.out.println("A");
+    else if (grade==3)
+    System.out.println("B");
+    else if (grade==2)
+    System.out.println("C");
+    else if (grade==1)
+    System.out.println("D");
+    else
+    System.out.println("F");
 
 
     
--- Prob4.java
+++ Prob4.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
-    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a temperature: ");
+    double temp= scan.nextDouble();
+
+    if (temp<=99 || >=102)
+    System.out.println("WARNING");
+
+    else
+    System.out.println("Temperature is OK");
 
 
     
